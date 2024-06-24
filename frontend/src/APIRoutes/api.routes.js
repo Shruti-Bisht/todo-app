bd518b5242e466b4967417e5d8e4b4ea48bdda1e
Shruti_Bisht <110077886+Shruti-Bisht@users.noreply.github.com>
2024-06-24 17:37:00 +0530

    Update api.routes.js

--- frontend/src/APIRoutes/api.routes.js
+++ frontend/src/APIRoutes/api.routes.js
@@ -1,5 +1,5 @@
 // export const  hostname="http://localhost:3000/api/v1"
-export const  hostname="https://todo-app-server-gold.vercel.app"
+export const  hostname="https://todo-app-server-gold.vercel.app/api/v1"
 
 export const authRegister=`${hostname}/auth/register`;
 export const authLogin=`${hostname}/auth/login`
