c26f0cee7384f1316aa98fdda3139e178aa824f0
Shruti_Bisht <110077886+Shruti-Bisht@users.noreply.github.com>
2024-06-24 19:17:00 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -17,7 +17,7 @@ app.get('/',(req,res)=>{
 
 // CORS configuration
 app.use(cors({
-  origin: process.env.CORS_ORIGIN, // Ensure CORS_ORIGIN is correctly set in your .env file
+  origin: 'http://localhost:5173/', // Ensure CORS_ORIGIN is correctly set in your .env file
   credentials: true, // Enable credentials (cookies, authorization headers) to be included in CORS requests
 }));
 
